1708c5346f93060de9687785bcbe84991ff7dcea
Hubert Brzóskniewicz <hubert.brzoskniewicz.stud@pw.edu.pl>
2022-03-23 13:58:31 +0100

    implemented quick sort

--- Lab2/sort.py
+++ Lab2/sort.py
@@ -1,6 +1,5 @@
 import sys
 import argparse
-from turtle import right
 
 
 def open_file(path):
@@ -56,7 +55,7 @@ def selection_sort(words):
     length = len(words)
     for i in range(length - 1):
         min = i
-        for j in range(i+1, length - 1):
+        for j in range(i+1, length):
             if words[j] < words[min]:
                 min = j
         words[i], words[min] = words[min], words[i]
@@ -64,6 +63,7 @@ def selection_sort(words):
 
 
 def merge_sort(words):
+    sorted_words = words
     if len(words) > 1:
         middle = len(words)//2
         left = words[:middle]
@@ -76,20 +76,40 @@ def merge_sort(words):
 
         while i < len(left) and j < len(right):
             if left[i] < right[j]:
-                words[k] = left[i]
+                sorted_words[k] = left[i]
                 i += 1
             else:
-                words[k] = right[j]
+                sorted_words[k] = right[j]
                 j += 1
             k += 1
         while i < len(left):
-            words[k] = left[i]
+            sorted_words[k] = left[i]
             i += 1
             k += 1
         while j < len(right):
-            words[k] = right[j]
+            sorted_words[k] = right[j]
             j += 1
             k += 1
+    return sorted_words
+
+
+def quick_sort(words):
+    less = []
+    equal = []
+    greater = []
+
+    if len(words) > 1:
+        pivot = words[0]
+        for x in words:
+            if x < pivot:
+                less.append(x)
+            elif x == pivot:
+                equal.append(x)
+            elif x > pivot:
+                greater.append(x)
+        return quick_sort(less)+equal+quick_sort(greater)
+    else:
+        return words
 
 
 def main(args):
@@ -102,8 +122,9 @@ def main(args):
     print(selection_sort(words[:1000]))
     print("--------------------------------")
     print(merge_sort(words[:1000]))
+    print("--------------------------------")
+    print(quick_sort(words[:1000]))
 
 
 if __name__ == "__main__":
     main(sys.argv[1:])
-# aa
